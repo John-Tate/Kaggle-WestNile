9e0fc264863e467ad2b3ee37b3d42bc4bed0aa30
John Tate <johntate@Johns-MacBook-Pro.local>
2016-06-14 15:51:27 -0400

    Computed vincent distance between test locations and weather stations, then assigned closest station to each test location

--- code/johnDataClean.py
+++ code/johnDataClean.py
@@ -4,25 +4,18 @@ weather = pd.read_csv('../assets/weather.csv')
 train = pd.read_csv('../assets/train.csv')
 spray = pd.read_csv('../assets/spray.csv')
 
-train.head()
-
-weather.head()
 
 station = pd.DataFrame({'Station':[1,2],'station_lat':[41.995,41.786],'station_lon':[-87.933,-87.752]})
 
 station_geo = [(41.995, -87.933),(41.786, -87.72)]
 
-station
 
 station['station_geo'] = station_geo
 weather2 = pd.merge(weather,station,on=['Station'])
 
-train.head()
-
-train['test_geo'] = train.Latitude.cat(others=[train.Longitude], sep=',')
-
 test_geo = []
 
+train.shape
 
 for index,item in train.iterrows():
     lat = str(item['Latitude'])
@@ -30,15 +23,11 @@ for index,item in train.iterrows():
     geo = '('+ lat +','+ lon +')'
     test_geo.append({'Latitude':item['Latitude'],'Longitude':item['Longitude'],'test_geo':geo})
 
-
-train.drop('test_geo', axis=1, inplace=True)
-
-
-
 train_geos = pd.DataFrame(test_geo)
 
+train_geos = train_geos.drop_duplicates()
 
-train = pd.merge(train,train_geos,on=['Latitude','Longitude'])
+train_geos
 
 station1 = station.station_geo[0]
 station2 = station.station_geo[1]
@@ -47,7 +36,7 @@ from geopy.distance import vincenty
 
 StationDistance = []
 
-for index,item in train.iterrows():
+for index,item in train_geos.iterrows():
     test_site = item['test_geo']
     s1Distance = vincenty(station1, test_site).miles
     s2Distance = vincenty(station2, test_site).miles
@@ -55,3 +44,11 @@ for index,item in train.iterrows():
         StationDistance.append({'test_geo':test_site,'Station':2})
     else:
         StationDistance.append({'test_geo':test_site,'Station':1})
+
+StationDistance = pd.DataFrame(StationDistance)
+
+train_geos = pd.merge(train_geos,StationDistance,on=['test_geo'])
+
+train = pd.merge(train,train_geos,on=['Latitude','Longitude'])
+
+train.head()
